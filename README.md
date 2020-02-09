60df3170dbf710223cb4c4ab8f1ba710b23b16f1
Alessandro Lou <alemau@uw.edu>
2020-02-09 12:19:02 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,23 @@
+<p align="center">
+    <img src="https://cbc-drupal-assets.s3.amazonaws.com/Cascade50-horiz-transp-440-110.png?Kbffbs4VZc0fjw7.r_UvaDMrGO9kdH2o" width="40%">
+</p>
+
+## Our Mission
+
+
+## Problem
+
+## Online Presence
+
+
+## Team members 
+- Eva Perez (PM)
+    - evagj@uw.edu
+- Jeongvin Yeon (PM/UX Design)
+    - yeomj@uw.edu
+- Kathy Tran (Engineer)
+    - @uw.edu
+- Alessandro Lou (Engineer)
+    - alemau@uw.edu
+
+## References
---
+++ bikelockerapp/database/admin.py
@@ -0,0 +1,24 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import *
+
+
+class StationA(admin.ModelAdmin):
+    list_display = ('station_name', 'station_county', 'station_zip')
+    list_filter = ('station_name', 'station_county', 'station_zip',)
+
+
+admin.site.register(Station, StationA)
+admin.site.register(Locker)
+admin.site.register(Loc_Maintenance)
+admin.site.register(Maintenance)
+admin.site.register(Maintenance_Type)
+admin.site.register(Customer)
+admin.site.register(Cust_Status)
+admin.site.register(Cust_Status_Type)
+admin.site.register(Cust_Loc)
+admin.site.register(Event_Type)
+admin.site.register(Event_Status)
+admin.site.register(Event)
+admin.site.register(Cust_Loc_Event)
