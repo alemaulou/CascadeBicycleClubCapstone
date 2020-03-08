6adf1c9a8ef8ea7f726ca8b3546489e68d5ca25f
Kathy Tran <kathylt@uw.edu>
2020-03-07 18:26:01 -0800

    switched to checkbox for locations in form

--- bikelockerapp/bikelockerapp/forms.py
+++ bikelockerapp/bikelockerapp/forms.py
@@ -4,8 +4,8 @@ from database.models import Customer, Location
 
 class CustomerForm(forms.ModelForm):
 
-    locations = forms.ModelMultipleChoiceField(queryset=Location.objects.all())
-                                      choices=lockerLocations)
+    locations = forms.ModelMultipleChoiceField(queryset=Location.objects.all(), widget=forms.CheckboxSelectMultiple)
+
     class Meta:
         model = Customer
         fields = ['cust_f_name', 'cust_l_name', 'cust_email', 'cust_address', 'cust_city', 'cust_state', 'cust_zip', 'locations']
