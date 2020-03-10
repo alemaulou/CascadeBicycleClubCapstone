8021ac54d6ae12f43a226495ef74278ac4802034
Kathy Tran <kathylt@uw.edu>
2020-03-10 16:58:42 -0700

    styled form with crispy forms

--- bikelockerapp/bikelockerapp/forms.py
+++ bikelockerapp/bikelockerapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column
 from database.models import Customer, Location
 
 class CustomerForm(forms.ModelForm):
@@ -9,3 +10,24 @@ class CustomerForm(forms.ModelForm):
     class Meta:
         model = Customer
         fields = ['cust_f_name', 'cust_l_name', 'cust_email', 'cust_address', 'cust_city', 'cust_state', 'cust_zip', 'locations']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column('cust_f_name', css_class='form-group col-md-6 mb-0'),
+                Column('cust_l_name', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
+            ),
+            'cust_address',
+            Row(
+                Column('cust_city', css_class='form-group col-md-6 mb-0'),
+                Column('cust_state', css_class='form-group col-md-4 mb-0'),
+                Column('cust_zip', css_class='form-group col-md-2 mb-0'),
+                css_class='form-row'
+            ),
+            'cust_email',
+            'locations',
+            Submit('submit', 'Submit')
+        )
