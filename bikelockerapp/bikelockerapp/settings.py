fea21b1505fd8817efc96dd8159bc4461add2359
Alessandro Lou <alemau@uw.edu>
2020-04-21 14:20:06 -0700

    Removing data

--- bikelockerapp/bikelockerapp/settings.py
+++ bikelockerapp/bikelockerapp/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # EMAIL
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'alessandro.lou@gmail.com'
-EMAIL_HOST_PASSWORD = 'alexmaris1'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
