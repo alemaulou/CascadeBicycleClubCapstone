3d43c8edde962f2badf9e81bd47af135cd68b74b
Alessandro Lou <alemau@uw.edu>
2020-05-12 11:11:11 -0700

    Minor change

--- bikelockerapp/bikelockerapp/settings.py
+++ bikelockerapp/bikelockerapp/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # EMAIL
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'alessandro.lou@gmail.com'
-EMAIL_HOST_PASSWORD = '2122dm15fH!'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
